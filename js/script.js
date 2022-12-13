5e66fef78d71594480a50f7b7d67cf428b4bb1b0
aldyfadil <aldifadilla883@gmail.com>
2022-12-13 21:18:15 +0700

    sudah jadi banh

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ const dragover_handler = ev =>{
 //Untuk Drag (seret)
 
 //Untuk Drop
-const drop_handler = ev =>{
+const Drop_handler = ev =>{
     console.log("drag")
     ev.preventDefault();
     
@@ -41,15 +41,55 @@ const dragend_handler = ev =>{
 //untuk drag
 
 //untuk method drop_handler
-//const drop_handler = ev => {
- //   console.log("drag")
-  //  ev.preventDefault();
+const drop_handler = ev => {
+    console.log("drag")
+   ev.preventDefault();
 
     //untuk dapat Posisi serta bisa drop end Drag
-  //  const data = ev.dataTransfer.getData("text/plain");
-   //     ev.target.innerText = document.getElementById(data).innerText;
-     //   document.getElementById(data).innerText = "";
+    const data = ev.dataTransfer.getData("text/plain");
+        ev.target.innerText = document.getElementById(data).innerText;
+       document.getElementById(data).innerText = "";
     //untuk dapat Posisi serta bisa drop end Drag
-//}
+}
+// select the list items
+let ul = document.querySelectorAll('li');;
+const letters= ["A", "B", "C", "D", "E", "F", "G", "H", ""]
+
+// this function sets a unique id for each list item, in the form 'li0' to 'li8'
+const setId = (items) => {
+    for(let i = 0; i < items.length; i++) {
+        items[i].setAttribute("id", `li${i}`)
+    }
+}
+
+const FillGrid = (items, letters) => {
+    items.forEach((item, i) => {
+        item.innerText = letters[i];
+    })
+}
+
+fillGrid(ul, letters);
 
 //untuk method drop_handler
+
+// shuffle the array
+const shuffle = (arr) => {
+    const copy = [...arr];
+    // loop over the array
+    for(let i = 0; i < copy.length; i++) {
+        // for each index,i pick a random index j 
+        let j = parseInt(Math.random()*copy.length);
+        // swap elements at i and j
+        let temp = copy[i];
+        copy[i] = copy[j];
+        copy[j] = temp;
+    }   
+    return copy;
+ }
+ const fillGrid = (items, letters) => {
+    let shuffled = shuffle(letters);
+
+    items.forEach((item, i) => {
+        item.innerText = shuffled[i];
+    })
+}
